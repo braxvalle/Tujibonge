f289747118ab3bf2bea8bc444e1258d63d6b397a
braxvalle <braxvalle2@gmail.com>
2024-01-24 21:01:16 +0300

    Create user model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is running from port 3000");
+app.listen(3001, () => {
+  console.log("server is running from port 3001");
 });
+
+app.use("/api/user", userRoutes);
