292ca16893481fd4511237db7a340e01d0943fd3
braxvalle <braxvalle2@gmail.com>
2024-01-25 20:31:16 +0300

    Created form ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/Tujibonge LOGO.png'
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+
+
   return (
     <div className='main-item'>
       <div className="form-items bg-white p-10">
         <div className="left-form">
           <img className='w-96' src={logo} alt="logo" />
-          <p className='w-2/4 py-8 text-xl'>Our community goal is always to save our Mental Health, We all need to be healthy and productive</p>
+          <p className='px-12 py-8 text-xl text-center'>Our community goal is always to save our Mental Health, We all need to be healthy and productive</p>
         </div>
         <div className="right-form">
-          <h1>Welcome to Our Community</h1>
+          <h2>Welcome to Our Community</h2>
           <form>
-            <div className='flex flex-col w-full'>
+            <div className='form-inputs flex flex-col gap-1 mb-5'>
               <label htmlFor="username">User Name</label>
               <input type="text" id='username' placeholder='Username' required  />
             </div>
-            <div className='flex flex-col w-full'>
+            <div className='form-inputs flex flex-col gap-1 mb-5'>
               <label htmlFor="email">Email</label>
               <input type="email" id='email' placeholder='name@company.com' required  />
             </div>
-            <div className='flex flex-col w-full'>
+            <div className='form-inputs flex flex-col gap-1 mb-5'>
               <label htmlFor="email">Password</label>
               <input type="password" id='password' placeholder='Enter password' required  />
             </div>
