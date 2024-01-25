1ff56945d7e227aff3796f036907532f37841354
braxvalle <braxvalle2@gmail.com>
2024-01-25 18:25:56 +0300

    input routes and database

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,28 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const harshedPassword = bcrypt.hashSync(password, 10);
+
+  const newUser = new User({ username, email, password: harshedPassword });
+
+  try {
+    await newUser.save();
+    res.json({ message: "signup success" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
